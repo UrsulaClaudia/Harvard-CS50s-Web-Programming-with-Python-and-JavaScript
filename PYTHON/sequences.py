ef12c89446b19b20e5591af0f67542caccf1bc23
UrsulaClaudia <claudiaursula.quispe@unmsm.edu.pe>
2025-02-09 11:53:56 -0500

    Basic Python lessons - Part 2

---
+++ PYTHON/sequences.py
@@ -0,0 +1,9 @@
+coordinateX  = 10.0
+coordinateY = 20.0
+coordinate = (10.0, 20.0)
+
+# Types of sequences
+# List
+# Tuple
+# Set
+# Dict
---
+++ PYTHON/squared.py
@@ -0,0 +1,4 @@
+import functions
+from functions import square
+for i in range(10):
+    print(f"The squared of {i} is {functions.square(i)}")
