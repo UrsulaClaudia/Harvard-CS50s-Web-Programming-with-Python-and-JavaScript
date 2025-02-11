553fff6189c0ec48d37ba230843df3e50f0588b8
UrsulaClaudia <claudiaursula.quispe@unmsm.edu.pe>
2025-02-10 20:38:39 -0500

    Creating 'Hello' project with Django

--- lecture3/hello/views.py
+++ lecture3/hello/views.py
@@ -3,4 +3,13 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world")
+    return render(request,"hello/index.html")
+
+def claudia(request):
+    return HttpResponse("Hello, Claudia")
+
+def samuel(request):
+    return HttpResponse("Hello, Samuel")
+
+def greet(request, name):
+    return render(request,"hello/greet.html",{"name":name.capitalize()})
